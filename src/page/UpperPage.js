6b43386bf397ec806f8579795b8b5d6a0845c2ea
USERNAME <EMAIL>
2022-07-08 09:09:59 +0000

    update url single quota

--- src/page/UpperPage.js
+++ src/page/UpperPage.js
@@ -31,7 +31,7 @@ function UpperPage() {
     const [ data, setState ] = useState({outcome:[]})
     
     // const url = '{backend-ingress ADDRESS}/services/all'
-    const url = 'http://k8s-default-backendi-6566bc7d31-137539812.ap-southeast-1.elb.amazonaws.com/services/all'
+    const url = `http://k8s-default-backendi-6566bc7d31-137539812.ap-southeast-1.elb.amazonaws.com/services/all`
     
     useEffect(() => {
       const fetchData = async () => {
